3a710691d1bd3fb2e6058766d3e2c62bef68d658
beee-cu-19bcs3810 <55451310+beee-cu-19bcs3810@users.noreply.github.com>
2019-09-19 21:13:45 +0530

    Add files via upload

---
+++ amazing_sango1.ino
@@ -0,0 +1,22 @@
+unsigned const LED = 8;
+unsigned const BUTTON = 7;
+unsigned int bState = 0;
+void setup()
+{
+  pinMode(BUTTON, INPUT);
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(BUTTON) == 1) { 
+  	digitalWrite(LED, HIGH);
+    bState += 1;
+    if(bState % 2 == 0){
+      digitalWrite(LED, LOW);
+      bState = 0;
+}
+    delay(100);
+  }
+  
+}
