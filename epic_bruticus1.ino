8eb276ca74d012c77ee1bacbea63ef9d6a812594
beee-cu-19bcs3810 <55451310+beee-cu-19bcs3810@users.noreply.github.com>
2019-09-19 19:24:04 +0530

    Add files via upload

---
+++ epic_bruticus1.ino
@@ -0,0 +1,26 @@
+int led = 13;
+int led2 = 12;
+int led3 = 11;
+void setup()
+{
+pinMode(led, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT); 
+}
+
+void loop()
+{
+digitalWrite(led, HIGH);  
+delay(100);               
+digitalWrite(led, LOW);    
+delay(100); 
+{digitalWrite(led2, HIGH);
+delay(100);
+digitalWrite(led2, LOW);
+delay(100);}
+{digitalWrite(led3, HIGH);
+delay(100);
+digitalWrite(led3, LOW);
+delay(100);
+}
+}
